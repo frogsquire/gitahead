7d273decfefad1a539c88f833f57f95d4cb53711
max <max@zimon.co>
2020-03-12 22:27:34 -0500

    add basic build batch script for windows (assumes strawberry perl)

--- build.bat
+++ build.bat
@@ -1,2 +1,7 @@
-﻿; TODO
+; builds in debug mode
+rmdir build\debug /s /q
+mkdir build\debug
+cd build\debug
 cmake -DCMAKE_IGNORE_PATH="C:/Strawberry/c/bin" -G Ninja -DCMAKE_BUILD_TYPE=Debug -DCMAKE_PREFIX_PATH=C:\Qt\Qt5.14.1\5.14.1\msvc2017_64 ../..
+ninja
+cd ../..
